26bf65c2af64bef708d27b92958f614216e7e096
Aleksa Stefanovic <aleksa.aki2005@gmail.com>
2024-02-19 09:05:55 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    import Question from "./Question.svelte";
+    let question;
+    const question;
+
+
+    const getQuestion=async()=>{
+        const res= await fetch("https://opentdb.com/api.php?amount=1&category=27&difficulty=easy&type=multiple")
+        let data = await res.json();
+
+    }
+    onMount(getQuestion);
+</script>
+<h1>My quiz</h1>
+{#if question !== undefined}
+<Question
+    text={question.question}
+    correctAnswer{question.correct_answer}
+    possibleAnswers{[...question.incorrect_answers,question.correct_answer]}
+    on:answers={getQuestion}
+/>
+{/if}
---
+++ src/routes/Question.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {createEventDispatcher} from "svelte";
+    export let text="";
+    export let correctAnswer;
+    export let possibleAnswers;
+    let selectedAnswer;
+    let state;
+    const shuffle=(array)=>{
+        return array.sort(()=>Math.random()-0.5);
+    };
+    const selectAnswer =(i)=>{
+        selectedAnswer=i;
+        if (selectedAnswer===correctIndex)
+            state="correct";
+        else
+            state="wrong";     
+    }
+    possibleAnswers=shuffle(possibleAnswers);
+    let correctIndex=possibleAnswers.indexOf(correctAnswer);
+</script>
+<div>
+    {#if state==="correct"}
+        <h2 style="color:green">{text}</h2>
+    {:else if state ==="wrong"}
+        <h2 style="color:red">{text}</h2>
+    {:else}
+        <h2>{text}</h2>
+    {/if}
+    {#each possibleAnswers as pa}
+        <button on:click={() => selectedAnswer(i)}>{pa}</button>
+    {/each}
+    
+</div>
